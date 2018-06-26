e4ef6a605e659f52602f66040e2814433b469dad
yanghong <yanghong@weilaixiansen.com>
2018-06-26 11:31:39 +0800

    2018-06-26 11-31

---
+++ browserTypePlug.js
@@ -0,0 +1,53 @@
+(function (plug){	
+
+		plug.wlxs       = new Object();
+
+		var SystemParam = {
+
+				 RegExpAgent : 	[
+												'MicroMessenger',
+												'Alipay',
+												'Chrome',
+				 ],
+
+				 outSign     : 	[
+												'weixin',
+												'alipay',
+												'chrome',
+				 ]
+		}
+		
+		var factory = function (){
+
+				var browser_user_agent = navigator.userAgent.toLowerCase(); 
+
+				var matchIndex = null;
+
+				var magent    =  SystemParam.RegExpAgent;
+
+				var pattern 	  = RegExp("");
+
+				for(var i in magent ){
+							 
+							 pattern.compile(magent[i],"i");
+
+							 if( pattern.test(browser_user_agent) ) 
+							 { 
+
+									matchIndex = i;
+
+									break;
+							 }
+
+				}
+
+				return  function (){
+								
+								return SystemParam.outSign[matchIndex];
+						}
+
+		}
+
+		plug.wlxs.getOpenWebPlatformWindow = factory();
+
+	})(window);
